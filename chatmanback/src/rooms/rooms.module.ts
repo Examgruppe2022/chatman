f3a7b05ccc97db0e6aad8489dd1b78731e6bb189
pete9991 <peter.lydeking@gmail.com>
2022-05-16 10:30:45 +0200

    added imports and providers

--- chatmanback/src/rooms/rooms.module.ts
+++ chatmanback/src/rooms/rooms.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RoomsService } from '../domain/services/rooms.service';
 import { RoomsController } from './rooms.controller';
+import { roomProvider } from '../infrastructure/mongoDB/roomProvider';
+import { userProvider } from '../infrastructure/mongoDB/userProvider';
+import { MongoDbModule } from '../infrastructure/mongoDB/mongoDB.module';
 
 @Module({
+  imports: [MongoDbModule],
   controllers: [RoomsController],
-  providers: [RoomsService],
+  providers: [RoomsService, ...roomProvider, ...userProvider],
 })
 export class RoomsModule {}
