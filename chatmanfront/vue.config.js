a46cd41f0c3c7e6c57a38028ee523a7ca9d1eac2
WackyGlow <stampe12@gmail.com>
2022-05-17 14:19:28 +0200

    Create vue.config.js

---
+++ chatmanfront/vue.config.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  pwa: {
+    name: "Chatman",
+    themeColor: "#98c1d9",
+    icons: {
+      favicon32: 'img/icons/ios/32.png',
+      favicon16: 'img/icons/ios/16.png',
+      appleTouchIcon: 'img/icons/ios/152.png',
+      maskIcon: 'img/icons/maskable.svg',
+      msTileImage: 'img/icons/ios/144.png'
+    }
+  }
+};
