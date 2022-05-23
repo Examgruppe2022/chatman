c35908a3733fbe85c494f1e42d5453e119f5006b
WackyGlow <stampe12@gmail.com>
2022-05-23 10:07:07 +0200

    jwt

---
+++ chatmanfront/src/helpers/auth-guard.ts
@@ -0,0 +1,12 @@
+import { accountService } from '@/_services';
+
+export function authGuard(to, from, next) {
+  const account = accountService.accountValue;
+
+  if (!account) {
+    // not logged in so redirect to login page with the return url
+    return next({ path: '/', query: { returnUrl: to.fullPath } });
+  }
+
+  next();
+}
